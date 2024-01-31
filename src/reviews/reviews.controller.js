8e75e943087ba18a15162f706d79de01c3a3960e
Malik <Mata@indeed.com>
2024-01-31 09:50:30 -0800

    Add review controller

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -2,21 +2,35 @@ const service = require("./reviews.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-async function reviewExists(request, response, next) {
-  // TODO: Write your code here
+async function reviewExists(req, res, next) {
+  const { reviewId } = req.params;
+  const review = await service.read(reviewId);
 
-  next({ });
+  if (review) {
+    res.locals.review = review;
+    return next();
+  }
+  return next({ status: 404, message: `Review cannot be found` });
 }
 
-async function destroy(request, response) {
-  // TODO: Write your code here
-
+async function destroy(req, res) {
+  const { review } = res.locals;
+  await service.delete(review.review_id);
+  res.sendStatus(204);
 }
 
-async function list(request, response) {
-  // TODO: Write your code here
+async function list(req, res) {
+  const { movieId } = req.params;
 
-  response.json({  });
+  // If a movieId is provided, fetch reviews for that specific movie.
+  if (movieId) {
+    const reviews = await service.listReviewsForMovie(movieId);
+    res.json({ data: reviews });
+  } else {
+    // If no movieId is provided, fetch all reviews.
+    const reviews = await service.listAllReviews();
+    res.json({ data: reviews });
+  }
 }
 
 function hasMovieIdInPath(request, response, next) {
@@ -33,11 +47,15 @@ function noMovieIdInPath(request, response, next) {
   next();
 }
 
-async function update(request, response) {
-  // TODO: Write your code here
+async function update(req, res) {
+  const { review } = res.locals;
+  const { update } = res.locals;
+  await service.update(update, review.review_id);
+  const updatedReview = await service.read(review.review_id);
+  const critic = await service.getCritic(review.critic_id);
 
+  res.status(200).json({ data: { ...updatedReview, critic: critic[0] } });
 }
-
 module.exports = {
   destroy: [
     noMovieIdInPath,
--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -1,11 +1,17 @@
 const service = require("./theaters.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
-async function list(request, response) {
-  // TODO: Add your code here
-  response.json({ });
+async function list(req, res) {
+  const theaters = await service.list();
+
+  for (let theater of theaters) {
+    const movies = await service.listMovies(theater.theater_id);
+
+    theater["movies"] = movies;
+  }
+  res.json({ data: theaters });
 }
 
 module.exports = {
-  list: asyncErrorBoundary(list),
+  list: [asyncErrorBoundary(list)],
 };
